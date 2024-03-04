4138bccc56021faeb473cde5b2f50540cbdad066
Alexander Roman <aroman.link11@gmail.com>
2024-03-04 11:09:00 -0500

    day_2.1

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ import time
 import json
 import requests
 
-from langchain_helper import get_qa_chain
-
+import dotenv
+from langchain.llms import GooglePalm
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 
 token_hugging_face = "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"
 
@@ -40,6 +45,55 @@ def recognize_speech(audio_file):
 
     return final_output
 
+@st.cache_resource()
+def get_model():
+
+    dotenv.load_dotenv()
+        
+    #Create Google Palm LLM model
+    llm = GooglePalm(maxOutputTokens = 200)
+
+    # Initialize instructor embeddings using the Hugging Face model
+    instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large",)
+
+    #Vector DB
+    vectordb_file_path = "macchu_picchu_vdb"
+
+    # Load the vector database from the local folder
+
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold = 0.7) #you can change score_threshold
+
+    prompt_template = """
+    As a travel guide expert on Machu Picchu, given the following context and a question, generate a conversational response following a conversation with an answer based solely on this context.
+    In the answer try to provide as much text as possible from "response" section in the source document context.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    {chat_history}
+    Human: {question}
+    Assistant:"""
+
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "chat_history", "question"]
+    )
+
+    chain_type_kwargs = {"prompt": PROMPT}
+
+    memory = ConversationBufferMemory(memory_key = f"chat_history", return_messages = True)
+
+
+    chain = ConversationalRetrievalChain.from_llm(llm=llm,
+                                chain_type="stuff",
+                                retriever=retriever,
+                                memory=memory, combine_docs_chain_kwargs=chain_type_kwargs)
+    
+    return chain
+
 
 def generate_answer(audio):
 
@@ -61,7 +115,7 @@ def generate_answer(audio):
 
 
         #Response question
-        chain = get_qa_chain()
+        chain = get_model()
 
         answer_guide = chain.run(text)
 
